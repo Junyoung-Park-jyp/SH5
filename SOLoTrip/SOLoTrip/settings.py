53f8337b01b53888c0129a67f24a49df317ccc1c
oodeng98 <oodeng98@naver.com>
2024-08-20 02:58:26 +0900

    feat: local에서 ec2 mysql server 연동

--- SOLoTrip/SOLoTrip/settings.py
+++ SOLoTrip/SOLoTrip/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()  # .env 파일 로드
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,16 +80,12 @@ WSGI_APPLICATION = 'SOLoTrip.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-        'default': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', '15.165.185.52'),
         'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
