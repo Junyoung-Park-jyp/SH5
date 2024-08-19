efbbcba4c3aa06eea5c71ccaf2086bccc536e00f
박준영 <raypark1241@gmail.com>
2024-08-19 18:21:13 +0900

    fix: store 제작중

--- Frontend/src/stores/travelStore.js
+++ Frontend/src/stores/travelStore.js
@@ -5,18 +5,22 @@ export const useTravelStore = defineStore('travel', {
     country: '',
     exchangeRate: null,
     members: [],
+    startDate: null,
+    endDate: null,
+    adjustTime: null,
   }),
 
   getters: {
-    travelCountry(state) {
+    country(state) {
       return state.country;
     },
-    travelExchangeRate(state) {
+    exchangeRate(state) {
       return state.exchangeRate;
     },
-    travelMembers(state) {
+    members(state) {
       return state.members
-    }
+    },
+    
   },
 
   actions: {
@@ -24,12 +28,18 @@ export const useTravelStore = defineStore('travel', {
       this.country = travelData.country;
       this.exchangeRate = travelData.exchangeRate;
       this.members = travelData.members;
+      this.startDate = travelData.startDate;
+      this.endDate = travelData.endDate;
+      this.adjustTime = travelData.adjustTime;
     },
 
     clearTravel() {
       this.country = '';
       this.exchangeRate = null;
       this.members = [];
+      this.startDate = null;
+      this.endDate = null;
+      this.adjustTime = null;
     },
   },
 });
