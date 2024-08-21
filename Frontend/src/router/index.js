507fb27d98b365a373002c29fa030664fa54df63
임광영 <kokoz22@naver.com>
2024-08-21 17:10:02 +0900

    feat: TripView 개발

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import BridgeView from "@/views/BridgeView.vue";
+import TripView from "@/views/TripView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/bridge",
       name: "bridge",
       component: BridgeView,
+    },
+    {
+      path: "/trip",
+      name: "trip",
+      component: TripView,
     },
   ],
 });
--- Frontend/src/views/TripView.vue
+++ Frontend/src/views/TripView.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-
-  </div>
+  <v-container>
+    {{ travelStore.travelExperiences }}
+    <v-row>
+      {{ userStore.name }} 님
+    </v-row>
+    <v-row>
+      <v-icon icon="mdi-music"></v-icon>
+      <div>현재 {{ travelStore.country }} 여행 중</div>
+      <v-icon icon="mdi-music"></v-icon>
+    </v-row>
+    <v-row>
+      총 0회 SOL로 여행을 다녀오셨네요!
+      <v-container>
+        <v-row>
+          <v-col></v-col>
+        </v-row>
+      </v-container>
+    </v-row>
+    <v-btn>여행 만들기</v-btn>
+  </v-container>
 </template>
 
 <script setup>
+import { useUserStore } from '@/stores/userStore';
+import { useTravelStore } from '@/stores/travelStore';
 
+const userStore = useUserStore()
+const travelStore = useTravelStore()
 </script>
 
 <style scoped>
