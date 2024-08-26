8594be115063cbfb0a8ac7c7feccfe768f8d2246
박준영 <raypark1241@gmail.com>
2024-08-26 23:05:18 +0900

    fix: axiosinstance 접근 방법 변경

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -3,7 +3,10 @@ import { useUserStore } from '@/stores/userStore';
 // Axios 인스턴스 생성
 const axiosInstance = axios.create({
   baseURL: 'http://52.79.246.151:8000',
-  timeout: 5000, 
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json'  // Content-Type 기본값 설정
+  } 
 });
 
 // // 요청 인터셉터 (추가적인 설정이 필요할 경우)
@@ -18,18 +21,5 @@ axiosInstance.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-
-// axiosInstance.interceptors.response.use(
-//   response => {
-//     return response;
-//   },
-//   error => {
-//     // 예를 들어, 401 에러에 대한 처리
-//     // if (error.response.status === 401) {
-//     //   // 로그아웃 또는 리프레시 토큰 로직
-//     // }
-//     return Promise.reject(error);
-//   }
-// );
 
 export default axiosInstance;
