257197ac8fdb99d90ad81d80509efc16c7be35bf
임광영 <kokoz22@naver.com>
2024-08-26 14:45:19 +0900

    feat: ChatGPT API Key를 활용한 django 요청

---
+++ chatgpt_api/api_test/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from openai import OpenAI
+
+# 환경 변수나 설정 파일에서 API 키를 가져오는 것이 안전합니다.
+OPENAI_API_KEY = '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'
+
+# openai.api_key = OPENAI_API_KEY
+
+@api_view(['GET'])
+def api(request):
+    if request.method == 'GET':
+        client = OpenAI(api_key=OPENAI_API_KEY)
+        # user_input = request.POST.get('user_input')
+        user_input = 'Hello'
+
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_input
+                    },
+            ]
+        )
+        answer = response.choices[0].message.content
+        print(answer)
+        return JsonResponse({'response': answer})
+    return JsonResponse({'error': 'Invalid request method.'}, status=400)
