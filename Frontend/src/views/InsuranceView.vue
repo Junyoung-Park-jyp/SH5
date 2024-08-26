dbf91562f8aaac9f2be40d907bfcf95cc8d236db
임광영 <kokoz22@naver.com>
2024-08-26 18:17:22 +0900

    feat: InsuranceMain 컴포넌트 생성

--- Frontend/src/views/InsuranceView.vue
+++ Frontend/src/views/InsuranceView.vue
@@ -10,10 +10,26 @@
     <!-- MAIN -->
     <div class="main px-2">
       <!-- 각 단계에 따른 폼을 조건부로 렌더링 -->
-       <InsuranceMain v-if="insuranceStage === 0" />
-       <InsuranceDetail v-else-if="insuranceStage === 1" />
-       <InsuranceMember v-else-if="insuranceStage === 2" />
-       <InsuranceInvite v-else />
+      <InsuranceMain v-if="insuranceStage === 0" />
+      <InsuranceDetail v-else-if="insuranceStage === 1" />
+      <InsuranceMember v-else-if="insuranceStage === 2" />
+      <InsuranceInvite v-else />
+    </div>
+
+    <!-- footer -->
+    <div class="sticky-container">
+      <div class="footer">
+        <div class="d-flex justify-space-between">
+          <div>
+            예산 보험료
+            <v-icon icon="mdi-help-circle-outline"></v-icon>
+          </div>
+          <div>
+            6000원
+          </div>
+        </div>
+        <v-btn @click="nextStep">다 음</v-btn>
+      </div>
     </div>
   </div>
 </template>
@@ -27,6 +43,10 @@ import InsuranceMember from '@/components/InsuranceView/insuranceMember.vue';
 import InsuranceInvite from '@/components/InsuranceView/insuranceInvite.vue';
 
 const insuranceStage = ref(0)
+
+const nextStep = () => {
+  insuranceStage.value++;
+}
 
 const backStep = () => { };
 </script>
@@ -50,5 +70,22 @@ const backStep = () => { };
   width: 100%;
   overflow-y: scroll;
   scrollbar-width: none;
+}
+
+/* footer */
+.sticky-container {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 1000;
+  margin: 0px auto;
+  padding: 0px;
+  background-color: white;  /* 추가: 배경색 지정으로 투명도를 방지 */
+  width: 100%;
+}
+
+.footer {
+  border-top: 1px solid #e0e0e0;
+  width: auto;
 }
 </style>
