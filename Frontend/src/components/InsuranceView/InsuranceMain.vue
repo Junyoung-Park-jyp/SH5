ab7f23d098dfd7e7a6946efe4f5775f38bdf82ac
임광영 <kokoz22@naver.com>
2024-08-26 15:41:12 +0900

    feat: 보험 페이지

---
+++ Frontend/src/components/InsuranceView/InsuranceMain.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>원하시는 보장을 선택해 주세요.</div>
+    <v-btn>추천해주세요</v-btn>
+    <v-btn>직접할게요</v-btn>
+    
+  </div>
+</template>
+
+<script setup>
+const products = {
+  '추천해주세요': [
+    ['기본형', '최소한의 보장이 필요한 분들', '5660원'],
+    ['고급형', '많은 보장이 필요한 분들', '12820원'],
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Frontend/src/views/InsuranceView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <!-- 뒤로가기 / 취소하기 -->
+    <div class="header my-2">
+      <div class="back">
+        <v-icon class="btns" icon="mdi-arrow-left" size="xx-large" @click="backStep"></v-icon>
+      </div>
+    </div>
+
+    <!-- MAIN -->
+    <div class="main px-2">
+      <!-- 각 단계에 따른 폼을 조건부로 렌더링 -->
+       <InsuranceMain v-if="insuranceStage === 0" />
+       <InsuranceDetail v-else-if="insuranceStage === 1" />
+       <InsuranceMember v-else-if="insuranceStage === 2" />
+       <InsuranceInvite v-else />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+import InsuranceMain from '@/components/InsuranceView/InsuranceMain.vue';
+import InsuranceDetail from '@/components/InsuranceView/InsuranceDetail.vue';
+import InsuranceMember from '@/components/InsuranceView/insuranceMember.vue';
+import InsuranceInvite from '@/components/InsuranceView/insuranceInvite.vue';
+
+const insuranceStage = ref(0)
+
+const backStep = () => { };
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 90vh;
+  margin: 0px auto;
+  padding: 10px 10px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.main {
+  margin: 30px auto 0px auto;
+  width: 100%;
+  overflow-y: scroll;
+  scrollbar-width: none;
+}
+</style>
