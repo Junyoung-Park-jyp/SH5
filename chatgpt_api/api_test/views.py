c2351d32dfff2ddb6ce73cc44e8bcebc0a5ab8dc
임광영 <kokoz22@naver.com>
2024-08-26 16:07:46 +0900

    feat: ChatGPT API를 활용한 결제 분류 기능

--- chatgpt_api/api_test/views.py
+++ chatgpt_api/api_test/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from django.http import JsonResponse
+from rest_framework.response import Response
+from django.conf import settings
+
 from openai import OpenAI
 
 @api_view(['GET'])
 def api(request):
     if request.method == 'GET':
-        client = OpenAI(api_key=OPENAI_API_KEY)
-        # user_input = request.POST.get('user_input')
-        user_input = 'Hello'
+        payment = request.GET.get('payment')
+
+        client = OpenAI(api_key=settings.OPENAI_API_KEY)
 
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model='gpt-4',
             messages=[
                 {
+                    'role': 'system',
+                    'content': '당신은 결제 내역을 보고 "카페", "외식", "생필품" "쇼핑" 등과 같은 특정 카테고리로 분류하는 도움을 주는 어시스트턴트입니다. 대답은 무조건 하나의 단어로만 해주세요.'
+                },
+                {
                     "role": "user",
-                    "content": user_input
-                    },
+                    "content": payment
+                },
             ]
         )
         answer = response.choices[0].message.content
-        print(answer)
-        return JsonResponse({'response': answer})
-    return JsonResponse({'error': 'Invalid request method.'}, status=400)
+        return Response({'category': answer})
+    return Response({'error': 'Invalid request method.'}, status=400)
--- chatgpt_api/chatgpt_api/settings.py
+++ chatgpt_api/chatgpt_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+OPENAI_API_KEY = env('OPENAI_API_KEY')
