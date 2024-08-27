22f824908b8b0bfa66b80a1dbff2d3fb4ffd13b5
임광영 <kokoz22@naver.com>
2024-08-27 12:53:40 +0900

    fix: generative AI 코드

--- generative_ai/AILab.py
+++ generative_ai/AILab.py
@@ -1,33 +1,71 @@
 import requests
+import json
+import time
 
-# url = 'https://www.ailabapi.com/api/image/effects/ai-anime-generator'
 
-# payload={'type': 'boy-5'}
-# files=[
-#   ('image',('file',open('./test.jpg','rb'),'application/octet-stream'))
-# ]
-# headers = {
-#   'ailabapi-api-key': ''
-# }
+AILAB_API_KEY = ''
 
-# response = requests.request("POST", url, headers=headers, data=payload, files=files)
+# 20 credits
+def portrait_animation(img_path: str) -> str:
+    url = 'https://www.ailabapi.com/api/portrait/effects/portrait-animation'
 
-# print(response.text)
+    payload = {
+        'type': 'pixar_plus',
+        }
+    files = [
+        ('image', ('file', open(img_path, 'rb'), 'application/octet-stream'))
+        ]
+    headers = {
+        'ailabapi-api-key': AILAB_API_KEY
+        }
+    
+    response = requests.request('POST', url, headers=headers, data=payload, files=files)
+    image_url = json.loads(response.text).get('data').get('image_url')
+    return image_url
 
-headers = {
-  'ailabapi-api-key': ''
-}
 
-# 요청할 URL
-url = 'https://www.ailabapi.com/api/common/query-async-task-result'
+# 10 credits
+def ai_anime_generator(img_path: str) -> str:
+    url_post = 'https://www.ailabapi.com/api/image/effects/ai-anime-generator'
 
-# 쿼리 파라미터 설정
-params = {
-    'task_id': '1724684240869.f4fb03a8-b905-e5cc-1713-c9707476d209'
-}
+    payload = {
+        'index': 0,  # Vintage Comic
+        # 'index': 1,  # 3D Fairy Tale
+        }
+    files = [
+        ('image', ('file', open(img_path, 'rb'), 'application/octet-stream'))
+        ]
+    headers = {
+        'ailabapi-api-key': AILAB_API_KEY
+        }
+    
+    response_post = requests.request('POST', url_post, headers=headers, data=payload, files=files)
 
-# GET 요청 보내기
-response = requests.get(url, headers=headers, params=params)
+    return json.loads(response_post.text).get('task_id')
 
-# 응답 상태 코드와 내용 출력
-print(response.text)
+
+def get_result(task_id: str) -> str:
+    url_get = 'https://www.ailabapi.com/api/common/query-async-task-result'
+
+    headers = {
+        'ailabapi-api-key': AILAB_API_KEY
+    }
+    params = {
+        'task_id': task_id
+    }
+
+    while True:
+        response_get = requests.request('GET', url_get, headers=headers, params=params)
+        response_json = json.loads(response_get.text)
+
+        task_status = response_json.get('task_status')
+        if task_status == 2:
+            image_url = response_json.get('data').get('result_url')
+            return image_url
+        elif task_status == 1:
+            print('Task is still processing.')
+            time.sleep(5)
+
+
+# print(portrait_animation('./test.JPG'))
+print(get_result(ai_anime_generator('./test.JPG')))
