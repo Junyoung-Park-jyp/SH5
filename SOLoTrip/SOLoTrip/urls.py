b03fe6c216db6e16e87e32d3522307f5dc8ed844
정태완 <oodeng98@naver.com>
2024-08-27 14:41:19 +0900

    fix: url 경로 수정

--- SOLoTrip/SOLoTrip/urls.py
+++ SOLoTrip/SOLoTrip/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('bank_accounts/', include('bank_accounts.urls')),
-    path('exchange_rates/', include('exchange_rates.urls')),
-    path('trips/', include('trips.urls')),
-    path('payments/', include('payments.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/bank_accounts/', include('bank_accounts.urls')),
+    path('api/exchange_rates/', include('exchange_rates.urls')),
+    path('api/trips/', include('trips.urls')),
+    path('api/payments/', include('payments.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
