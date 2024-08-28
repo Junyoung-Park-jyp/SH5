b8c2fbd166546b6bc50033b693a0b7a35a08df47
AndCookie <jamie9@naver.com>
2024-08-28 18:15:15 +0900

    Merge

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,10 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import { resolve } from 'path'
 
-dotenv.config({ path: resolve(__dirname, '../.env') });
+// dotenv.config({ path: resolve(__dirname, '../.env') });
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,10 +19,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  define: {
-    'import.meta.env': {
-      ...import.meta.env,
-      VITE_AILABAPI_API_KEY: process.env.VITE_AILABAPI_API_KEY,
-    }
-  }
+  // define: {
+  //   'import.meta.env': {
+  //     ...import.meta.env,
+  //     VITE_AILABAPI_API_KEY: process.env.VITE_AILABAPI_API_KEY,
+  //   }
+  // }
 })
