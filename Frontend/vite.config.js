fb212f5620f4cfd054b8f4056fa9f09b20ba8929
임광영 <kokoz22@naver.com>
2024-08-28 16:45:45 +0900

    fix: .env 파일의 루트 디렉토리 적용 수정

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,6 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import dotenv from 'dotenv'
+import { resolve } from 'path'
+
+dotenv.config({ path: resolve(__dirname, '../.env') });
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +17,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  define: {
+    'import.meta.env': {
+      ...import.meta.env,
+      VITE_AILABAPI_API_KEY: process.env.VITE_AILABAPI_API_KEY,
     }
   }
 })
