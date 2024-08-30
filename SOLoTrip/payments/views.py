5cbf24308b74b2b41cef2b74bb47108647e9d299
정태완 <oodeng98@naver.com>
2024-08-30 10:18:03 +0900

    feat: pay_list 기능 요청 변수 변경

--- SOLoTrip/payments/views.py
+++ SOLoTrip/payments/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import get_user_model
 from .models import Payment, Calculate
-from trips.models import Member
+from trips.models import Member, Trip
 from .serializers import PaymentCreateSerializer, PaymentDetailSerializer, CalculateCreateSerializer
 from shinhan_api.demand_deposit import update_demand_deposit_account_withdrawal as withdrawal
 from shinhan_api.demand_deposit import update_demand_deposit_account_Transfer as transfer
@@ -17,7 +17,6 @@ User = get_user_model()
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def pay(request):
-    # pay_date, pay_time에 bank_account 계좌에서 brand_name에서 결제한 amount가 출금된다
     if request.method == 'POST':
         data = request.data
         bank_account = data.get('bank_account')
@@ -33,7 +32,6 @@ def pay(request):
         data['transaction_unique_number'] = response['REC']['transactionUniqueNo']
         data['transaction_type'] = '출금'
         
-        # ChatGPT API를 이용하여 결제 내역의 카테고리를 저장한다
         try:
             data['category'] = categorize(data.get('brand_name'))
         except:
@@ -49,10 +47,10 @@ def pay(request):
 @permission_classes([IsAuthenticated])
 def pay_list(request):
     if request.method == 'GET':
-        # start_date~finish_date 까지의 trip_id와 연계된 bank_account 계좌'들'의 거래 내역을 조회한다
         trip_id = request.GET.get('trip_id')
-        start_date = request.GET.get('start_date')
-        end_date = request.GET.get('end_date')
+        trip = Trip.objects.get(pk=trip_id)
+        start_date = trip.start_date
+        end_date = trip.end_date
         
         if not Member.objects.filter(trip=trip_id, user=request.user).exists():
             return Response({'error': "현재 사용자는 해당 여행에 참여하지 않았습니다."}, status=status.HTTP_401_UNAUTHORIZED)
@@ -73,8 +71,6 @@ def pay_list(request):
 @permission_classes([IsAuthenticated])
 def adjustment(request):
     if request.method == 'POST':
-        # trip_id에 떠있는 결제 내역을 클릭하면 정산할 수 있고, 멤버별 값을 결정할 수 있다.
-        # 필요 데이터: trip_id, {member별 정산 금액과 bank_account}, payment_id
         payments = request.data.get('payments')
         for payment in payments:    
             payment_id = payment.get('payment_id')
@@ -94,8 +90,6 @@ def adjustment(request):
 @permission_classes([IsAuthenticated])
 def objection(request):
     if request.method == 'POST':
-        # trip_id에 떠있는 결제 내역을 클릭하면 취소할 수 있다.
-        # 필요 데이터: payment_id
         trip_id = request.data.get('trip_id')
         payment_id = request.data.get('payment_id')
         calculates = Calculate.objects.filter(payment=payment_id)
@@ -114,7 +108,6 @@ def objection(request):
         withdrawal_user = Member.objects.filter(bank_account=withdrawal_bank_account)[0].user
         withdrawal_email = withdrawal_user.email
         username = withdrawal_user.username
-        # result[username] = {"before_balance": int(balance(withdrawal_email, withdrawal_bank_account)['REC']['accountBalance'])}
         for calculate in calculates:
             deposit_user = calculate.member
             deposit_bank_account = deposit_user.bank_account
